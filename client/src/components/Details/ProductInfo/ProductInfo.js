5f8673a86246efa02b2696ad3ffef1016cc3c8ef
TH1117 <tjchswkd@naver.com>
2023-06-08 21:46:57 +0900

    태훈_폰트 오류 수정  + 상품 페이지 수정 중

--- client/src/components/Details/ProductInfo/ProductInfo.js
+++ client/src/components/Details/ProductInfo/ProductInfo.js
@@ -32,40 +32,66 @@ function ProductInfo({ params }) {
 
     return (
         <>
-            <Image className="col-lg-12" src={params.image} rounded />
-            <Row>
-                <h1 className="col-lg-10 col-sm-10 product-info-heading">{params.title}</h1>
-                <span id="heartIconDetails" className="col-lg-1 col-sm-1" onClick={onHearthClick}>
-                {params.isAuth && <>
-                    {!wish ? (
-                        <OverlayTrigger placement="top" overlay={<Tooltip>Add to Wishlist</Tooltip>}>
-                            <BsHeart />
-                        </OverlayTrigger>
-                    )
-                        : (
-                            <OverlayTrigger placement="top" overlay={<Tooltip>Remove from Wishlist</Tooltip>}>
-                                <BsHeartFill />
+            <div className="d-flex flex-column align-items-center">
+                <Image className="col-lg-12" src={params.image} rounded />
+                <hr />
+                <Row>
+                    <section id='profile'>
+                        <a id='profile_link'>
+                            <div className='space-beteen'>
+                                <div style={{ flex: 1 }}>
+                                    <div id='profile_image'>
+                                        {params.name}
+                                    </div>
+                                    <div id='profile_left'>
+                                        {params._id}
+                                    <div id='profile_address'>주소가 들어갈 자리</div>
+                                    </div>
+                                </div>
+                                <div className='profile_right'>
+                                    <dl className='manner_temper'>
+                                        <dt>매너온도</dt>
+                                    </dl>
+                                </div>
+                            </div>
+                        </a>
+                    </section> 
+                </Row>
+
+                <Row>
+                    <h5 className="-sm-12 product-info-heading">{params.title}</h5>
+                    <span id="heartIconDetails" className="col-lg-1 col-sm-1" onClick={onHearthClick}>
+                    {params.isAuth && <>
+                        {!wish ? (
+                            <OverlayTrigger placement="top" overlay={<Tooltip>Add to Wishlist</Tooltip>}>
+                                <BsHeart />
                             </OverlayTrigger>
                         )
-                    }
-                </>}
+                            : (
+                                <OverlayTrigger placement="top" overlay={<Tooltip>Remove from Wishlist</Tooltip>}>
+                                    <BsHeartFill />
+                                </OverlayTrigger>
+                            )
+                        }
+                    </>}
 
-                </span>
-            </Row>
-            <div id="detailsCardText" className="col-lg-12">
-                <Tabs defaultActiveKey="details" transition={false}>
-                    <Tab eventKey="details" title="Details" id="tab-details">
-                        {params.description}
-                        <hr />
-                        <p id="details-footer" className="text-muted">Product listed at {params.addedAt}</p>
-                    </Tab>
-                    {/* <Tab eventKey="aboutSeller" title="About seller">
-                        <p>Name: {params.name || "Not specified"}</p>
-                        <p>Email: {params.email}</p>
-                        <p>Telephone: {params.phone}</p>
-                        <p>City: {params.city}</p>
-                    </Tab> */}
-                </Tabs>
+                    </span>
+                </Row>
+                <div id="detailsCardText" className="col-lg-12">
+                    <Tabs defaultActiveKey="details" transition={false}>
+                        <Tab eventKey="details" title="Details" id="tab-details">
+                            {params.description}
+                            <hr />
+                            <p id="details-footer" className="text-muted">Product listed at {params.addedAt}</p>
+                        </Tab>
+                        {/* <Tab eventKey="aboutSeller" title="About seller">
+                            <p>Name: {params.name || "Not specified"}</p>
+                            <p>Email: {params.email}</p>
+                            <p>Telephone: {params.phone}</p>
+                            <p>City: {params.city}</p>
+                        </Tab> */}
+                    </Tabs>
+                </div>
             </div>
         </>
     )
