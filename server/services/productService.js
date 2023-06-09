371b88b64d79432b288edb135e5ee4a1455d6b30
ksqrt <ksqrt98@gmail.com>
2023-06-09 12:32:39 +0900

    s3 에 업로드 but 사진이 깨짐

--- server/services/productService.js
+++ server/services/productService.js
@@ -1,9 +1,15 @@
 const Product = require('../models/Product');
 const User = require('../models/User');
-const { cloudinary } = require('../config/cloudinary');
 const { S3 } = require('../config/aws-sdk');
+const { v4: uuidv4 } = require('uuid');
+
+
+const { cloudinary } = require('../config/cloudinary');
 const { CLOUDINARY_STORAGE } = require('../config/config');
 const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+
 
 async function getAll() {
     return await Product.paginate();
@@ -30,37 +36,59 @@ async function create(data, userId) {
 
 
 
+
+// async function uploadImage(image) {
+//     const uploadResponse = await cloudinary.uploader.upload(image, {
+//         upload_preset: CLOUDINARY_STORAGE,
+//     }, { quality: "auto" });
+
+//     let imageUrl = uploadResponse.url;
+//     let index = (imageUrl.indexOf('upload/')) + 6;
+
+//     let compressedImg = imageUrl
+//         .substring(0, index) +
+//         "/c_fit,q_auto,f_auto,w_800" +
+//         imageUrl.substring(index);
+
+//     return compressedImg;
+// }
+
 async function uploadImage(image) {
-  const fileContent = fs.readFileSync(image);
+    const bucket_name = 'ncp3';
+    const object_name = `sample-folder/${uuidv4()}.jpg`;
 
-  const params = {
-    Bucket: 'ncp3',
-    Key: 'your-file-name.jpg', // S3에 저장될 파일 이름
-    Body: fileContent,
-    ACL: 'public-read', // 업로드된 이미지를 퍼블릭으로 설정하려는 경우
-  };
+    try {
+        // Convert image to Buffer
+        const imageBuffer = Buffer.from(image, 'binary');
 
-  const result = await S3.upload(params).promise();
-  console.log('Image uploaded successfully. URL:', result.Location);
-  return result.Location;
+        // Upload file to S3
+        const uploadParams = {
+            Bucket: bucket_name,
+            Key: object_name,
+            ACL: 'public-read',
+            Body: imageBuffer
+        };
+        const uploadResult = await S3.upload(uploadParams).promise();
+
+        // 업로드된 이미지 URL 생성
+        const imageUrl = uploadResult.Location;
+
+        // 이미지 URL을 원하는 형식으로 변환 (여기서는 리사이징 예시)
+        const index = imageUrl.indexOf('upload/') + 7;
+        const resizedImageUrl =
+            imageUrl.substring(0, index) +
+            'c_fit,q_auto,f_auto,w_800' +
+            imageUrl.substring(index);
+
+        return imageUrl;
+
+    } catch (error) {
+        console.error('S3 image upload failed:', error);
+        throw error;
+    }
 }
 
 
-async function uploadImage(image) {
-    const uploadResponse = await cloudinary.uploader.upload(image, {
-        upload_preset: CLOUDINARY_STORAGE,
-    }, { quality: "auto" });
-
-    let imageUrl = uploadResponse.url;
-    let index = (imageUrl.indexOf('upload/')) + 6;
-
-    let compressedImg = imageUrl
-        .substring(0, index) +
-        "/c_fit,q_auto,f_auto,w_800" +
-        imageUrl.substring(index);
-
-    return compressedImg;
-}
 
 async function userCollectionUpdate(userId, product) {
     return await User.updateOne({ _id: userId }, { $push: { createdSells: product } });
