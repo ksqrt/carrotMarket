f7c0e43337568fe44d39b0022ca16c12acb37a0c
ksqrt <ksqrt98@gmail.com>
2023-06-12 16:53:43 +0900

    S3 이미지 성공

---
+++ client/src/Pages/ImageTest.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const ImageTest = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageUpload = async () => {
+    if (selectedImage) {
+      const reader = new FileReader();
+      reader.readAsDataURL(selectedImage);
+      reader.onloadend = async () => {
+        const base64data = reader.result;
+
+        try {
+          await axios.post('http://localhost:5000/imageTest', { image: base64data });
+          console.log('Image uploaded successfully!');
+        } catch (error) {
+          console.error('Error uploading image:', error);
+        }
+      };
+    }
+  };
+
+  const handleImageSelect = (event) => {
+    setSelectedImage(event.target.files[0]);
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleImageSelect} />
+      <button onClick={handleImageUpload}>Upload</button>
+    </div>
+  );
+};
+
+
+export default ImageTest;
---
+++ server/controllers/imageTestController.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+// const isAuth = require('../middlewares/isAuth');
+// const isGuest = require('../middlewares/isGuest');
+const { SECRET, COOKIE_NAME } = require('../config/config');
+const jwt = require('jsonwebtoken');
+const productService = require('../services/productService');
+
+
+router.post('/', async (req, res) => {
+    try {
+
+
+        let compressedImg = await productService.uploadImage(req.body.image);
+
+        res.status(200).json({ success });
+    } catch (error) {
+        res.status(500).json({ error });
+    }
+})
+
+module.exports = router;
