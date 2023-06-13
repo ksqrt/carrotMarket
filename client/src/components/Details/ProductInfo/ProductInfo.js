ca75508b485e07e147e427c6c1b5d0acf3bce795
TH1117 <tjchswkd@naver.com>
2023-06-13 14:52:28 +0900

    Revert "프로필 온도 수정"
    This reverts commit 25654972aa6ca5a0ebd9815a2065f581358bd8e2.

--- client/src/components/Details/ProductInfo/ProductInfo.js
+++ client/src/components/Details/ProductInfo/ProductInfo.js
@@ -35,7 +35,7 @@ function ProductInfo({ params }) {
       .catch(err => console.log(err));
   };
 
-  //글 작성시간 계산 함수...
+  //글 작성시간 계산 함수
   const displayCreateAt = (createdAt) => {
     const date = new Date(createdAt);
     const now = Date.now();
