8ade01c7a02064bc9dda42a5f7e3e55441b60efd
zknight666 <55696400+zknight666@users.noreply.github.com>
2023-06-13 15:17:45 +0900

    socket.io 수정 중

--- client/src/components/Details/ProductInfo/ProductInfo.js
+++ client/src/components/Details/ProductInfo/ProductInfo.js
@@ -1,11 +1,16 @@
-import { useState, useEffect } from 'react';
-import { Row, Tabs, Tab, Image, OverlayTrigger , Tooltip} from 'react-bootstrap';
+import { useState, useEffect, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Row, Tabs, Tab, Image, OverlayTrigger , Tooltip, Button} from 'react-bootstrap';
 import { BsHeart, BsHeartFill } from 'react-icons/bs';
 import { wishProduct } from '../../../services/productData'
+import { startChat, initializeSocket } from '../../../services/messagesData';
+import { Context } from '../../../ContextStore';
 
 function ProductInfo({ params }) {
     const [wish, setWish] = useState(false);
-
+    const [socket, setSocket] = useState(null);
+    const { userData } = useContext(Context);
+    const history = useHistory();
     useEffect(() => {
         if (params.isWished === true) {
             setWish(true)
@@ -30,8 +35,31 @@ function ProductInfo({ params }) {
         }
     }
 
+    useEffect(() => {
+        const initSocket = async () => {
+            const socket = await initializeSocket();
+            setSocket(socket);
+
+            socket.on('startChat', ({ chatId }) => {
+                history.push(`/messages/${chatId}`);
+            });
+        };
+
+        initSocket();
+    }, []);
+      
+      const onChatStart = async (e) => {
+        e.preventDefault();
+        if (!socket) return;
+        startChat(socket, { buyerId: userData._id, sellerId: params.sellerId });
+      };
+
+
+
+
     return (
         <>
+        
             <Image className="col-lg-12" src={params.image} rounded />
             <Row>
                 <h1 className="col-lg-10 col-sm-10 product-info-heading">{params.title}</h1>
@@ -52,6 +80,7 @@ function ProductInfo({ params }) {
 
                 </span>
             </Row>
+            <Button variant="dark" onClick={onChatStart}>Chat Start</Button>;
             <div id="detailsCardText" className="col-lg-12">
                 <Tabs defaultActiveKey="details" transition={false}>
                     <Tab eventKey="details" title="Details" id="tab-details">
