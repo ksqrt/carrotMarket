48da053d3f22712234b26d004bc3ccfa46d08df4
sunghunaaa <121652427+sunghunaaa@users.noreply.github.com>
2023-06-13 15:25:21 +0900

    pull

--- client/src/Pages/Details.js
+++ client/src/Pages/Details.js
@@ -42,6 +42,10 @@ function Details({ match, history }) {
                             <Aside params={product} history={history} />
                         </Col>
                     </Row></>) : (<Spinner animation="border" />)}
+                <a id="kakaotalk-sharing-btn" href="javascript:;">
+                    <img src="https://developers.kakao.com/assets/img/about/logos/kakaotalksharing/kakaotalk_sharing_btn_medium.png"
+                    alt="카카오톡 공유 보내기 버튼" />
+                </a>
             </div>
         </>
     )
