775ab73a27344538e62ebcf652aa1edaef14ec5f
ksqrt <ksqrt98@gmail.com>
2023-06-13 14:56:07 +0900

    버튼 바꾸기

--- client/src/components/Categories/CategoriesNav.js
+++ client/src/components/Categories/CategoriesNav.js
@@ -5,6 +5,7 @@ import {
   BsHouseDoorFill,
   BsFillHouseFill,
   BsFillPuzzleFill,
+  
 } from "react-icons/bs";
 import { AiFillCar } from "react-icons/ai";
 import { GiFlowerPot, GiClothes } from "react-icons/gi";
@@ -23,30 +24,73 @@ function CategoriesNav() {
         </Button>{" "}
       </Link>
       
+      <Link to="/categories/electronics">
+        <Button variant="dark" id="electronics">
+          <GiClothes />
+          의류
+        </Button>{" "}
+      </Link>
+
+      <Link to="/categories/properties">
+        <Button variant="dark" id="properties">
+          <MdPhoneAndroid />
+          가전제품
+        </Button>{" "}
+      </Link>
+
       <Link to="/categories/properties">
         <Button variant="dark" id="properties">
           <BsHouseDoorFill />
-          Properties
+          가구/인테리어
         </Button>{" "}
       </Link>
+      <Link to="/categories/properties">
+        <Button variant="dark" id="properties">
+          <BsHouseDoorFill />
+          자동차 및 오토바이
+        </Button>{" "}
+      </Link>
+      <Link to="/categories/properties">
+        <Button variant="dark" id="properties">
+          <BsHouseDoorFill />
+          스포츠 및 레저용품
+        </Button>{" "}
+      </Link>
+
+      <Link to="/categories/properties">
+        <Button variant="dark" id="properties">
+          <BsHouseDoorFill />
+          아동용품
+        </Button>{" "}
+      </Link>
+      <Link to="/categories/properties">
+        <Button variant="dark" id="properties">
+          <BsHouseDoorFill />
+          도서 및 문구용품
+        </Button>{" "}
+      </Link>
+      <Link to="/categories/properties">
+        <Button variant="dark" id="properties">
+          <BsHouseDoorFill />
+          신발
+
+        </Button>{" "}
+      </Link>
+
       <Link to="/categories/auto">
         <Button variant="dark" id="auto">
           <AiFillCar />
-          자동차
+          악세서리 및 장신구
         </Button>{" "}
       </Link>
+
       <Link to="/categories/home">
         <Button variant="dark" id="home">
-          <BsFillHouseFill />집
+          <BsFillHouseFill />뷰티 및 화장품
         </Button>{" "}
       </Link>
-      <Link to="/categories/electronics">
-        <Button variant="dark" id="electronics">
-          <MdPhoneAndroid />
-          디지털 기기
-        </Button>{" "}
-      </Link>
-      <Link to="/categories/clothes">
+      
+      {/* <Link to="/categories/clothes">
         <Button variant="dark" id="clothes">
           <GiClothes />
           의류
@@ -63,7 +107,7 @@ function CategoriesNav() {
           <GiFlowerPot />
           식물
         </Button>{" "}
-      </Link>
+      </Link> */}
     </div>
   );
 }
