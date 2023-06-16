db46e59df875a89921a57c5a30d5fe748bf5373c
sunghunaaa <121652427+sunghunaaa@users.noreply.github.com>
2023-06-16 17:57:31 +0900

    풀 전 커밋

--- client/src/Pages/Details.js
+++ client/src/Pages/Details.js
@@ -38,10 +38,10 @@ function Details({ match, history }) {
                             <ProductInfo params={product} />
                         </Col>
                     </Row></>) : (<Spinner animation="border" />)}
-                <a id="kakaotalk-sharing-btn" href="javascript:;">
+                {/* <a id="kakaotalk-sharing-btn" href="javascript:;">
                     <img src="https://developers.kakao.com/assets/img/about/logos/kakaotalksharing/kakaotalk_sharing_btn_medium.png"
                     alt="카카오톡 공유 보내기 버튼" />
-                </a>
+                </a> */}
             </div>
         </>
     )
