57fef77e06fe2f5851567a7249a3969ec8a9940a
Jun <rnsuz74@naver.com>
2023-06-22 21:46:13 +0900

    카카오 맵 api 연결 성공!

--- client/src/components/KakaoMapAPI/KakaoMapAPI.js
+++ client/src/components/KakaoMapAPI/KakaoMapAPI.js
@@ -6,7 +6,7 @@ const KakaoMapAPI = () => {
 	const [location, setLoacation] = useState({latitude: null, longitude: null}); // 현재 위치를 저장할 상태
 	const [address, setAddress] = useState(null); // 현재 좌표의 주소를 저장할 상태
 
-	//현재 위치를 불러오는것
+	//현재 위치를 불러옴
 	useEffect(() => {
 		navigator.geolocation.getCurrentPosition(successHandler, errorHandler); // 성공시 successHandler, 실패시 errorHandler 함수가 실행된다.
 	}, []);
@@ -21,7 +21,7 @@ const KakaoMapAPI = () => {
 		console.log(error);
 	};
 
-	//현재위치의 위도 경도값을 기반으로 주소로 변경해주는것
+	//현재위치의 위도, 경도값을 기반으로 주소값으로 변경
 	const getAddress = (lat, lng) => {
 		const geocoder = new kakao.maps.services.Geocoder(); // 좌표 -> 주소로 변환해주는 객체
 		const coord = new kakao.maps.LatLng(location.latitude, location.longitude); // 주소로 변환할 좌표 입력
@@ -53,53 +53,6 @@ const KakaoMapAPI = () => {
 			)}
 		</>
 	);
-  const getPosSuccess = (pos) => {
-    const currentPos = new window.kakao.maps.LatLng(
-      pos.coords.latitude,
-      pos.coords.longitude
-    );
-    if (map && marker) {  // ensure that map and marker are not null
-        map.panTo(currentPos);
-
-        marker.setMap(null);
-        marker.setPosition(currentPos);
-        marker.setMap(map);
-    }
-};
-
-  useDidMountEffect(() => {
-    window.kakao.maps.event.addListener(map, "click", function (mouseEvent) {
-      var geocoder = new window.kakao.maps.services.Geocoder();
-
-      geocoder.coord2Address(
-        mouseEvent.latLng.getLng(),
-        mouseEvent.latLng.getLat(),
-        function (result, status) {
-          if (status === window.kakao.maps.services.Status.OK) {
-            var addr = !!result[0].road_address
-              ? result[0].road_address.address_name
-              : result[0].address.address_name;
-
-            console.log(addr);
-
-            if (marker) {  // ensure that marker is not null
-                marker.setMap(null);
-                marker.setPosition(mouseEvent.latLng);
-                marker.setMap(map);
-            }
-          }
-        }
-      );
-    });
-  }, [map]);
-
-  return (
-    <div>
-      <div id="map" style={{ width: '500px', height: '500px' }}></div>
-      <div onClick={getCurrentPosBtn}>현재 위치</div>
-      
-    </div>
-  );
 };
 
 export default KakaoMapAPI;
